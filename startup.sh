d3036e32c10aee71c621a5f627baa530b5c97536
Nubl1simo <84527362+Nubl1simo@users.noreply.github.com>
2022-05-22 08:53:02 +0200

    Delete startup.sh

--- startup.sh
+++
@@ -1,8 +0,0 @@
-xset r rate 300 45
-xcompmgr &
-xrandr --output HDMI-3 --left-of HDMI-1
-xwallpaper --zoom $HOME/dox/media/pix/wallpapers/wallpaper.jpg # change this to wherever your wallpaper might be
-sxhkd &
-fcitx &
-$HOME/dox/sh/dwm-bar/dwm-bar.sh &
-$HOME/dox/sh/keyboard-remapping/commands &
